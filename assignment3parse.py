d4e63336c0096cbd587bd8ac76d698aecf037931
czconnolly <c.z.connolly@dundee.ac.uk>
2014-03-13 17:32:16 +0000

    using regular expression now

--- assignment3parse.py
+++ assignment3parse.py
@@ -9,41 +9,58 @@ infile='/homes/czconnolly/BS32010/assignment3/ig_pairs.out'
 
 fh = open(infile)
 
+
+import re
+
+index = {}
+identifier = {}
+
 line = fh.readline()
-while line != ' 1542   >d4jvpa_/1-132         132 b.1.1.1 (A:) automated matches {Vicugna pacos [TaxId: 30538]         0	0.00	   ':
-	line=fh.readline()
 
-header= fh.readline().strip()
+regexp=r'(\d+) +>([^/]+)/'
+m=re.search(regexp,line)
+print m.group(1)
+#m.group(2)
 
-colnames = {}
-
-index=0
-for title in header.split('\t'):
-	colnames[title]=index
-	print '%s\t%s'%(title,index)
-	index=index+1
+#line = outfile.readline()
+#while line = regex
+#	line=fh.readline()
+#index = m.group(1)
+#identifier = m.group(2)
 
 
-igfile=open('ig_file.txt','w')
 
-columns=['I', 'J','ILEN', 'JLEN', 'MATCH', 'NGAPS', 'NALIG', 'NIDENT', '%IDENT', 'NAS', 'NASAL', 'NRANS', 'RMEAN', 'STDEV', 'SCORE']
+#header= fh.readline().strip()
 
-def buildrow(row, fields):
-	newrow=[]
-	for f in fields:
-		newrow.append(row[int(colnames[f])])
-	return '\t'.join(newrow)+'\n'
+#colnames = {}
 
-for line in fh.readlines():
-	try:
-		if line[0] == 'I':
-			continue
-		row=line.strip().split('\t')
-		igfile.write(buildrow(row, columns))
-		rows=rows+1
-	except:
-		pass
+#index=0
+#for title in header.split('\t'):
+#	colnames[title]=index
+#	print '%s\t%s'%(title,index)
+#	index=index+1
 
-igfile.close()
 
-print '%s rows processed'%rows
+#igfile=open('ig_file.txt','w')
+
+#columns=['I', 'J','ILEN', 'JLEN', 'MATCH', 'NGAPS', 'NALIG', 'NIDENT', '%IDENT', 'NAS', 'NASAL', 'NRANS', 'RMEAN', 'STDEV', 'SCORE']
+
+#def buildrow(row, fields):
+#	newrow=[]
+#	for f in fields:
+#		newrow.append(row[int(colnames[f])])
+#	return '\t'.join(newrow)+'\n'
+
+#for line in fh.readlines():
+#	try:
+#		if line[0] == 'I':
+#			continue
+#		row=line.strip().split('\t')
+#		igfile.write(buildrow(row, columns))
+#		rows=rows+1
+#	except:
+#		pass
+
+#igfile.close()
+
+#print '%s rows processed'%rows
