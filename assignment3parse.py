d4b9a09878d7eb0c71007cb2e944fabbae6f73d0
czconnolly <c.z.connolly@dundee.ac.uk>
2014-03-14 14:26:19 +0000

    succesfully strip 2nd data table

--- assignment3parse.py
+++ assignment3parse.py
@@ -14,40 +14,51 @@ mydict = {}
 
 line = fh.readline()
 
-#for line in fh:
-#	regexp=r'(\d+) +>([^/]+)/'
- #       m=re.search(regexp,line)
-#	if m:
-#		group1 = m.group(1)
-#		group2 = m.group(2)
+for line in fh:
+	regexp=r'(\d+) +>([^/]+)/'
+        m=re.search(regexp,line)
+	if m:
+		group1 = m.group(1)
+		group2 = m.group(2)
 		
-#		index.append(group1)
-#		identifier.append(group2)
-#		mydict = {'index':index, 'identifier':identifier}
+		index.append(group1)
+		identifier.append(group2)
+		mydict = {'index':index, 'identifier':identifier}
 #print mydict		
 
 
-Ifield = []
+I = []
+J = []
+ILEN = []
+JLEN = []
+MATCH = []
+NGAPS = []
+NALIG = []
+NIDENT = []
+IDENT = []
+NAS = []
+NASAL = []
+NRANS = []
+RMEAN = []
+STDEV = []
+SCORE = []
+
 for line in fh:
-	Ifield.append(line[0:5].strip())
-print Ifield
-
-
-
-#J = line[6:10].strip()
-#ILEN =line[11:15].strip()
-#JLEN = line[16:20].strip()
-#MATCH = line[21:30].strip()
-#NGAPS = line[31:37].strip()
-#NALIG = line[38:44].strip()
-#NIDENT = line[45:51].strip()
-#IDENT = line[52:61].strip()
-#NAS = line[62:71].strip()
-#NASAL = line[72:81].strip()
-#NRANS = line[82:88].strip()
-#RMEAN = line[89:98].strip()
-#STDEV = line[99:108].strip()
-#SCORE = line[109:119].strip()
+	I.append(line[0:5].strip())
+	J.append(line[6:10].strip())
+	ILEN.append(line[11:15].strip())
+	JLEN.append(line[16:20].strip())
+	MATCH.append(line[21:30].strip())
+	NGAPS.append(line[31:37].strip())
+	NALIG.append(line[38:44].strip())
+	NIDENT.append(line[45:51].strip())
+	IDENT.append(line[52:61].strip())
+	NAS.append(line[62:71].strip())
+	NASAL.append(line[72:81].strip())
+	NRANS.append(line[82:88].strip())
+	RMEAN.append(line[89:98].strip())
+	STDEV.append(line[99:108].strip())
+	SCORE.append(line[109:119].strip())
 
 
 
